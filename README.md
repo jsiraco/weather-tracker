b048f7fffaf9abb9eed5e7bad32a208f8b43f9b0
Jas <ragnarock.josef@gmail.com>
2021-08-09 15:57:45 -0400

    Finished features, updated readMe

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # weather-tracker
+## Purpose 
+The purpose of this application is to provide the user with a simple weather dashboard. In this app they will be able to enter a city and and the app will display the daily forecast as well as a five-day forecast.
+## Tools
+This application was built using JQuery, OpenWeatherAPI, and Bulma
+### Future development
+For future development, the biggest goal is to make the user experience more appealing with graphics and images
+## Links 
+A link to the live application can be found
+### *Here:*
+https://jsiraco.github.io/weather-tracker/
+A link to the repositiory can be found
+### *Here:*
+https://github.com/jsiraco/weather-tracker
+### Screenshots
+![Weather_App_Screenshot](https://user-images.githubusercontent.com/83827081/128766119-3fb44799-f614-4352-a2ff-aadf23e54cbe.png)
+![A screenshot of a simple weather app with a list of saved locations, a daily forecast, and a five-day forecast]
--- assets/script.js
+++ assets/script.js
@@ -8,8 +8,10 @@ const forecastResl = $(".forecast");
 const APIKey = "c1141616fb1714998cc67977ee400134";
 const searchBtn = $("#search-button");
 
-let forecastResults = [];
 let recentCities = [];
+
+// recentCities.push("Ogunquit");
+// console.log(recentCities);
 
 const testWeather = [
     { "coord": { "lon": 139.6917, "lat": 35.6895 }, "weather": [{ "id": 802, "main": "Clouds", "description": "scattered clouds", "icon": "03d" }], "base": "stations", "main": { "temp": 87.26, "feels_like": 97.25, "temp_min": 83.75, "temp_max": 90.79, "pressure": 1007, "humidity": 68 }, "visibility": 10000, "wind": { "speed": 1.99, "deg": 131, "gust": 5.01 }, "clouds": { "all": 40 }, "dt": 1627862701, "sys": { "type": 2, "id": 2001249, "country": "JP", "sunrise": 1627847380, "sunset": 1627897515 }, "timezone": 32400, "id": 1850144, "name": "Tokyo", "cod": 200 }
@@ -22,8 +24,6 @@ function init() {
         recentCities = storedCities;
     }
     buildList(recentCities);
-    console.log(recentCities);
-
 }
 
 function clearText() {
@@ -31,16 +31,20 @@ function clearText() {
 }
 
 function buildToday(response) {
-    let today = JSON.stringify(response.main);
-    let tempContent = $("<p>").html(today);
-    let todayDiv = $("<div>").addClass("content").append(tempContent);
+    let cityH = $("<h2>").addClass("subtitle").text(JSON.stringify(response.name));
+    let today = "Temp: " + JSON.stringify(response.main.temp) + ", Feels Like: " + JSON.stringify(response.main.feels_like) + ", Humidity: " + JSON.stringify(response.main.humidity) + ", Conditions: " + JSON.stringify(response.weather[0].main);
+    let tempContent = $("<p>").text(today);
+    let todayDiv = $("<div>").addClass("content").append(cityH, tempContent);
     todayResults.html(todayDiv);
 }
 
 function buildWeekly(response) {
     forecastResl.html("")
-    for (let i = 0; i < 5; i++) {
-        let forecast = JSON.stringify(response.list[i].main);
+    //every 8 hours
+    //40 total hours 
+    //Open weather charges to do a daily forecast, so an hourly forecast was used then incremented by 8 hours to avoid get data from each day and avoid the pay wall
+    for (let i = 0; i <= 40; i += 8) {
+        let forecast = JSON.stringify(response.list[i].dt_txt) + " " + JSON.stringify(response.list[i].main.temp) + ", Conditions: " + JSON.stringify(response.list[i].weather[0].main);
         let forecastContent = $("<p>").html(forecast);
         let forecastDiv = $("<div>").addClass("content").append(forecastContent);
         forecastResl.append(forecastDiv);
@@ -48,21 +52,10 @@ function buildWeekly(response) {
     }
 }
 
-function storeCity(response) {
-    if (searchEl === "") {
-        return;
-    }
-    $(recentCities).push(JSON.stringify(response))
-    localStorage.setItem("recents", JSON.stringify(response));
-}
 
-//builds list
-function buildList(response) {
-    //let data = JSON.parse(localStorage.getItem("recents"));
-    //$(data).each((item) => {
+function buildLastSearch(response) {
     let prevSearchBtn = $("<button>").addClass("button is-link is-outlined is-fullwidth").html(response);
     recentSearch.append(prevSearchBtn);
-
 
     $(prevSearchBtn).on("click", function () {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${response}&units=imperial&appid=${APIKey}`)
@@ -72,6 +65,9 @@ function buildList(response) {
             .then(function (response) {
                 buildToday(response);
             })
+            .catch(function() {
+                console.log("");
+            })
         fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${response}&units=imperial&appid=${APIKey}`)
             .then(function (response) {
                 return response.json();
@@ -79,13 +75,48 @@ function buildList(response) {
             .then(function (response) {
                 buildWeekly(response);
             })
+            .catch(function() {
+                console.log("");
+            })
     })
-    //})
+}
+
+//builds list
+function buildList() {
+    for (let i = 0; i < recentCities.length; i++) {
+        let prevSearchBtn = $("<button>").addClass("button is-link is-outlined is-fullwidth").html(recentCities[i]);
+        recentSearch.append(prevSearchBtn);
+
+        $(prevSearchBtn).on("click", function () {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${recentCities[i]}&units=imperial&appid=${APIKey}`)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (response) {
+                    buildToday(response);
+                })
+                .catch(function() {
+                    console.log("");
+                })
+            fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${recentCities[i]}&units=imperial&appid=${APIKey}`)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (response) {
+                    buildWeekly(response);
+                })
+                .catch(function() {
+                    console.log("");
+                })
+        })
+    }
 }
 
 ///Builds forecast
 $(searchBtn).on("click", function () {
-    if (searchEl === "") {
+    if (searchEl.val() === "") {
+        alert("enter a city");
+        window.location.reload();
         return;
     }
     let city = searchEl.val();
@@ -95,8 +126,14 @@ $(searchBtn).on("click", function () {
             return response.json();
         })
         .then(function (response) {
+            recentCities.push(city);
+            console.log(recentCities);
+            localStorage.setItem("recents", JSON.stringify(recentCities));
             buildWeekly(response);
             clearText();
+        })
+        .catch(function() {
+            console.log("");
         })
 })
 
@@ -107,7 +144,6 @@ $(searchBtn).on("click", function () {
         return;
     }
     let city = searchEl.val();
-    console.log("LOOK HERE-------", recentCities);
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`)
         .then(function (response) {
@@ -115,14 +151,12 @@ $(searchBtn).on("click", function () {
         })
         .then(function (response) {
             buildToday(response);
-            buildList(response.name);
-            storeCity(response.name);
-            console.log("NOW LOOK HERE-------", recentCities);
+            buildLastSearch(response.name);
             clearText();
         })
-    // .catch(function () {
-    //     console.log("Error");
-    // })
+        .catch(function() {
+            console.log("");
+        })
 })
 
 
